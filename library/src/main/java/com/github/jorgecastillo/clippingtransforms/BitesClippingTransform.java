c80a855d3e97196619fba6c3e08c79d439a1629f
Jorge Castillo <jorge.castillo.prz@gmail.com>
2015-08-14 10:04:10 +0200

    Little fixes.

--- library/src/main/java/com/github/jorgecastillo/clippingtransforms/BitesClippingTransform.java
+++ library/src/main/java/com/github/jorgecastillo/clippingtransforms/BitesClippingTransform.java
@@ -26,58 +26,55 @@ import android.view.View;
  */
 public class BitesClippingTransform implements ClippingTransform {
 
-    private float roundedEdgeHeight = 32f;
-    private int waveCount = 8;
-    private int width, height;
+  private float roundedEdgeHeight = 32f;
+  private int waveCount = 8;
+  private int width, height;
 
-    public BitesClippingTransform() {
+  public BitesClippingTransform() {
+  }
+
+  public BitesClippingTransform(float roundedEdgeHeight, int waveCount) {
+    this.roundedEdgeHeight = roundedEdgeHeight;
+    this.waveCount = waveCount;
+  }
+
+  @Override public void transform(Canvas canvas, float currentFillPhase, View view) {
+    cacheDimensions(view.getWidth(), view.getHeight());
+    Path path = buildClippingPath();
+    path.offset(0, height * -currentFillPhase);
+    canvas.clipPath(path, Region.Op.DIFFERENCE);
+  }
+
+  private void cacheDimensions(int width, int height) {
+    if (this.width == 0 || this.height == 0) {
+      this.width = width;
+      this.height = height;
+    }
+  }
+
+  protected Path buildClippingPath() {
+    Path roundedPath = new Path();
+
+    float widthDiff = width * 1f / (waveCount * 2);
+
+    float startingHeight = height;
+    roundedPath.moveTo(0, startingHeight);
+
+    float nextCPX = widthDiff;
+    float nextCPY = startingHeight + roundedEdgeHeight;
+    float nextX = nextCPX + widthDiff;
+
+    for (int i = 0; i < waveCount; i++) {
+      roundedPath.quadTo(nextCPX, nextCPY, nextX, startingHeight);
+      nextCPX = nextX + widthDiff;
+      nextX = nextCPX + widthDiff;
     }
 
-    public BitesClippingTransform(float roundedEdgeHeight, int waveCount) {
-        this.roundedEdgeHeight = roundedEdgeHeight;
-        this.waveCount = waveCount;
-    }
+    roundedPath.lineTo(height + 100, startingHeight);
+    roundedPath.lineTo(height + 100, 0);
+    roundedPath.lineTo(0, 0);
+    roundedPath.close();
 
-
-
-    @Override
-    public void transform(Canvas canvas, float currentFillPhase, View view) {
-        cacheDimensions(view.getWidth(), view.getHeight());
-        Path path = buildClippingPath();
-        path.offset(0, height * -currentFillPhase);
-        canvas.clipPath(path, Region.Op.DIFFERENCE);
-    }
-
-    private void cacheDimensions(int width, int height) {
-        if (this.width == 0 || this.height == 0) {
-            this.width = width;
-            this.height = height;
-        }
-    }
-
-    protected Path buildClippingPath() {
-        Path roundedPath = new Path();
-
-        float widthDiff = width * 1f / (waveCount * 2);
-
-        float startingHeight = height;
-        roundedPath.moveTo(0, startingHeight);
-
-        float nextCPX = widthDiff;
-        float nextCPY = startingHeight + roundedEdgeHeight;
-        float nextX = nextCPX + widthDiff;
-
-        for (int i = 0; i < waveCount; i++) {
-            roundedPath.quadTo(nextCPX, nextCPY, nextX, startingHeight);
-            nextCPX = nextX + widthDiff;
-            nextX = nextCPX + widthDiff;
-        }
-
-        roundedPath.lineTo(height + 100, startingHeight);
-        roundedPath.lineTo(height + 100, 0);
-        roundedPath.lineTo(0, 0);
-        roundedPath.close();
-
-        return roundedPath;
-    }
+    return roundedPath;
+  }
 }
--- library/src/main/java/com/github/jorgecastillo/clippingtransforms/SquareClippingTransform.java
+++ library/src/main/java/com/github/jorgecastillo/clippingtransforms/SquareClippingTransform.java
@@ -11,51 +11,50 @@ import android.view.View;
  */
 public class SquareClippingTransform implements ClippingTransform {
 
-    private int width, height;
-    private int squareSize = 24;
+  private int width, height;
+  private int squareSize = 24;
 
-    public SquareClippingTransform() { }
+  public SquareClippingTransform() {
+  }
 
-    public SquareClippingTransform(int squareSize) {
-        this.squareSize = squareSize;
+  public SquareClippingTransform(int squareSize) {
+    this.squareSize = squareSize;
+  }
+
+  @Override public void transform(Canvas canvas, float currentFillPhase, View view) {
+    cacheDimensions(view.getWidth(), view.getHeight());
+    Path path = buildClippingPath();
+    path.offset(0, height * -currentFillPhase);
+    canvas.clipPath(path, Region.Op.DIFFERENCE);
+  }
+
+  private void cacheDimensions(int width, int height) {
+    if (this.width == 0 || this.height == 0) {
+      this.width = width;
+      this.height = height;
+    }
+  }
+
+  protected Path buildClippingPath() {
+
+    Path squaresPath = new Path();
+    int numSquares = (int) Math.ceil(width / (2 * squareSize));
+    int startingHeight = height;
+    int lowerHeight = startingHeight - squareSize;
+    squaresPath.moveTo(0, startingHeight);
+
+    for (int i = 0; i < numSquares; i++) {
+      squaresPath.lineTo((2 * i + 1) * squareSize, startingHeight);
+      squaresPath.lineTo((2 * i + 1) * squareSize, lowerHeight);
+      squaresPath.lineTo(2 * (i + 1) * squareSize, lowerHeight);
+      squaresPath.lineTo(2 * (i + 1) * squareSize, startingHeight);
     }
 
-    @Override
-    public void transform(Canvas canvas, float currentFillPhase, View view) {
-        cacheDimensions(view.getWidth(), view.getHeight());
-        Path path = buildClippingPath();
-        path.offset(0, height * -currentFillPhase);
-        canvas.clipPath(path, Region.Op.DIFFERENCE);
-    }
+    squaresPath.lineTo(width, startingHeight);
+    squaresPath.lineTo(width, 0);
+    squaresPath.lineTo(0, 0);
+    squaresPath.close();
 
-    private void cacheDimensions(int width, int height) {
-        if (this.width == 0 || this.height == 0) {
-            this.width = width;
-            this.height = height;
-        }
-    }
-
-    protected Path buildClippingPath() {
-
-        Path squaresPath = new Path();
-        int numSquares = (int) Math.ceil(width / (2 * squareSize));
-        int startingHeight = height;
-        int lowerHeight = startingHeight - squareSize;
-        squaresPath.moveTo(0, startingHeight);
-
-        for (int i = 0; i < numSquares; i++) {
-            squaresPath.lineTo((2 * i + 1) * squareSize, startingHeight);
-            squaresPath.lineTo((2 * i + 1) * squareSize, lowerHeight);
-            squaresPath.lineTo(2 * (i + 1) * squareSize, lowerHeight);
-            squaresPath.lineTo(2 * (i + 1) * squareSize, startingHeight);
-        }
-
-        squaresPath.lineTo(width, startingHeight);
-        squaresPath.lineTo(width, 0);
-        squaresPath.lineTo(0, 0);
-        squaresPath.close();
-
-        return squaresPath;
-    }
-
+    return squaresPath;
+  }
 }
